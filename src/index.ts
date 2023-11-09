aa33b3423ff3c2ee8db5cad696c7f46b797a0102
Alexandria Roberts <iamlexieonearth@gmail.com>
2023-11-09 10:34:44 +0800

    created a user class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('Hi there');
+import { User } from './models/User';
+
+const user = new User({ name: 'LExie', age: 38 });
+
+user.set({ name: 'newName', age: 999 });
+console.log(user.get('name'));
+console.log(user.get('age'));

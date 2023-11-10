75100c5d4a5fc76d414c6c15a239dc8067d9bf31
Alexandria Roberts <iamlexieonearth@gmail.com>
2023-11-10 23:56:30 +0800

    on and trigger methods for events

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -127,12 +127,27 @@ exports.User = void 0;
 var User = /** @class */function () {
   function User(data) {
     this.data = data;
+    this.events = {};
   }
   User.prototype.get = function (propName) {
     return this.data[propName];
   };
   User.prototype.set = function (update) {
     Object.assign(this.data, update);
+  };
+  User.prototype.on = function (eventName, callback) {
+    var handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  };
+  User.prototype.trigger = function (eventName) {
+    var handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+    handlers.forEach(function (callback) {
+      callback();
+    });
   };
   return User;
 }();
@@ -148,12 +163,13 @@ var user = new User_1.User({
   name: 'LExie',
   age: 38
 });
-user.set({
-  name: 'newName',
-  age: 999
+user.on('change', function () {
+  console.log('change');
 });
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('hi', function () {
+  console.log('hi');
+});
+user.trigger('change');
 },{"./models/User":"src/models/User.ts"}],"../../.nvm/versions/node/v16.15.1/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -179,7 +195,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50011" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52679" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];

3a2c505891f071d6f69f061030495d87095c4715
Alexandria Roberts <iamlexieonearth@gmail.com>
2023-11-11 00:01:12 +0800

    on and trigger methods for events

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import { User } from './models/User';
 
 const user = new User({ name: 'LExie', age: 38 });
 
-user.set({ name: 'newName', age: 999 });
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {
+  console.log('change');
+});
+
+user.on('hi', () => {
+  console.log('hi');
+});
+
+user.trigger('change');

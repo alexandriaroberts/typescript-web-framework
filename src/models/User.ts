0816f1359f5c3835032869db55ee0a66a4371b89
Alexandria Roberts <iamlexieonearth@gmail.com>
2023-11-17 21:42:45 +0800

    Attribute class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 export interface UserProps {
   id?: number;
@@ -11,13 +12,5 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
+  public attributes: Attributes<UserProps> = new Attributes<UserProps>();
 }

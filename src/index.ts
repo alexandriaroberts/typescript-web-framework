373847bcf63a97071e0eb56c3b47c48a19482d86
Alexandria Roberts <iamlexieonearth@gmail.com>
2023-11-18 15:02:01 +0800

    integration of Attributes class in User class

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'new', age: 0 });
+const user = new User({ name: 'new record', age: 0 });
 
-// user.fetch();
-
-// setTimeout(() => {
-//   console.log(user);
-// }, 4000);
-
-// user.set({ name: 'Lexie', age: 30 });
-
-user.save();
-
-user.events.on('change', () => {
-  console.log('change');
-});
-
-user.events.trigger('change');
+user.attributes.get('id');
+user.sync.save();

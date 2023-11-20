db4de553a90cdc1d166a3c25409999a8529c0a2f
Alexandria Roberts <iamlexieonearth@gmail.com>
2023-11-20 22:26:19 +0800

    working on get accessors to reimplement methods

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
 
-user.attributes.get('id');
-user.sync.save();
+user.on('change', () => {
+  console.log('user was changed');
+});

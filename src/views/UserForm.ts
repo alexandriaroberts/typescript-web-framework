3d4c629105b8aa32487a089e9e6b908f528a9dca
Alexandria Roberts <iamlexieonearth@gmail.com>
2024-01-16 22:27:16 +0800

    customising userForm class

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -5,8 +5,13 @@ export class UserForm extends View<User, UserProps> {
     return {
       'click:.set-age': this.onSetAgeClick,
       'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
     };
   }
+
+  onSaveClick = (): void => {
+    this.model.save();
+  };
 
   onSetNameClick = (): void => {
     const input = this.parent.querySelector('input');
@@ -22,12 +27,11 @@ export class UserForm extends View<User, UserProps> {
   template(): string {
     return `
         <div>
-          <h1>User Form</h1>
-          <p>User name: ${this.model.get('name')}</p>
-          <p>User age: ${this.model.get('age')}</p>
-          <input />
+     
+          <input placeholder="${this.model.get('name')}"/>
           <button class="set-name">Change Name</button>
           <button class="set-age">Set random age</button>
+          <button class="save-model">Save User</button>
         </div>
       `;
   }
